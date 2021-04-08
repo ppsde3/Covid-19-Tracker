6f92c07b33653c443a0c22eecd48d27a64e40283
Prashant Pandey <67046339+ppsde3@users.noreply.github.com>
2021-04-08 23:52:19 +0530

    Updates

---
+++ precache-manifest.f776d7ebc3177f67d2e7d0694b879688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0eafea51975537c8903ca90d5f5c60",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "8ad5f988285425cf8dfe",
+    "url": "/Covid-19-Tracker/static/css/2.38ca0958.chunk.css"
+  },
+  {
+    "revision": "e5dc59a6966af9a9753d",
+    "url": "/Covid-19-Tracker/static/css/main.2dd75947.chunk.css"
+  },
+  {
+    "revision": "8ad5f988285425cf8dfe",
+    "url": "/Covid-19-Tracker/static/js/2.967662ef.chunk.js"
+  },
+  {
+    "revision": "b2fa9c9ab95d8aec7657827ff0419aef",
+    "url": "/Covid-19-Tracker/static/js/2.967662ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5dc59a6966af9a9753d",
+    "url": "/Covid-19-Tracker/static/js/main.a354a7c9.chunk.js"
+  },
+  {
+    "revision": "fe4ce01d5d90c4505ae1",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.a0ec76f1.js"
+  }
+]);
